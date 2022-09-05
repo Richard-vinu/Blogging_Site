6b736c577799f9515b420f8cb49464519031da99
miryala veronica <101985218+veronica17-web@users.noreply.github.com>
2022-09-05 16:02:04 +0530

    adding index

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+let route = require('./src/router/router')
+const bodyParser = require('body-parser');
+const { default: mongoose } = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb+srv://miryala-veronica123:Blw0rAVvkhRhMsxq@cluster0.vfkx3.mongodb.net/project1-grp62?retryWrites=true&w=majority", {
+    useNewUrlParser: true
+})
+.then( () => console.log("MongoDb is connected"))
+.catch ( err => console.log(err) )
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000, function () {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
