65e47b8016d61b8512a603abf8a7ee3d39eb8888
Richard-vinu <richardnova1999@gmail.com>
2022-09-05 22:22:18 +0530

    getBlogByQuery

--- src/controller/blogcontroller.js
+++ src/controller/blogcontroller.js
@@ -6,4 +6,45 @@ let createblog = async function(req,res){
     let savedata = await blogModel.create(data)
     res.send({data:savedata})
 }
-module.exports.createblog=createblog
+
+
+
+
+
+
+const getBlogByQuery = async function (req, res) {
+    try {
+      let authId = req.query.authorId;
+      let cat = req.query.category;
+      let subcat = req.query.subcategory;
+      let tag = req.query.tags;
+  
+      let allData = await blogModel
+        .find({
+          isDeleted: false,
+          isPublished: true,
+          $or: [
+            { authorId: authId },
+            { category: cat },
+            { subcategory: subcat },
+            { tags: tag },
+          ],
+        })
+        .populate("authorId");
+  
+      //*Validation
+  
+      if (allData.length == 0)
+        return res.status(404).send({ msg: "Enter valid Details" });
+      res.status(200).send({ status: true, msg: allData });
+    } catch (err) {
+      res
+        .status(500)
+        .send({ status: false, msg: "server Error", err: err.message });
+    }
+  };
+
+
+
+
+module.exports = {createblog,getBlogByQuery}
--- src/router/router.js
+++ src/router/router.js
@@ -2,10 +2,11 @@ const express = require('express')
 
 const router = express.Router()
 const {createAuthor,loginAuthor}= require('../controller/authorController.js')
-const {createblog}= require("../controller/blogcontroller")
+const {createblog,getBlogByQuery}= require("../controller/blogcontroller")
 
 
 router.post('/authors',createAuthor)
 router.post("/login",loginAuthor)
 router.post('/createblog',createblog)
+router.get('/blogs',getBlogByQuery)
 module.exports = router
