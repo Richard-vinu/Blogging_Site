6c7400f30a07f9c659cc7c0b11d0624782b393f0
miryala veronica <101985218+veronica17-web@users.noreply.github.com>
2022-09-05 19:55:45 +0530

    updated with edge cases

--- src/controller/authorController.js.js
+++ src/controller/authorController.js.js
@@ -6,27 +6,29 @@ const createAuthor = async (req,res)=>{
     try{
 
         let result = req.body
-        let {fname,lname,title,email}=result
-        if(!fname){res.send({msg:"fname is mandatory"})}
-        else if(!lname){res.send({msg:"fname is mandatory"})}
-
+        let {fname,lname,title,email,password}=result
+        if(!fname){res.status(400).send({msg:"fname is mandatory"})}
+         if(!lname){res.status(400).send({msg:"lname is mandatory"})}
+         if(!title){res.status(400).send({msg:"title is mandatory"})}
+         if(!email){res.status(400).send({msg:"email is mandatory"})}
+         if(!password){res.status(400).send({msg:"password is mandatory"})}
    const data = await authorModel.create(result)
 
    res.status(201).send({data:data})
 
     }catch(err){
         res.status(500).send({status:false,error:err.message})
-    }
+    } 
 }
 const loginAuthor = async (req,res)=>
 {
     try{
         // let {email} =req.body
         // let {password}=req.body
-     let data = req.body
+     let data = req.body 
      let {email,password } = data
-        if(!email){res.send({msg:"email id is mandatory"})   }
-        if(!password){res.send({msg:"password is mandatory"})}
+        if(!email){res.status(400).send({msg:"email id is mandatory"})   }
+        if(!password){res.status(400).send({msg:"password is mandatory"})}
         let authorCheck = await authorModel.findOne({ email: email, password: password})
         if (!authorCheck) { return res.status(401).send("Incorrect email or password.") } 
 
