a3255881387b3bbe5d50ae3458660f0ded291183
Richard-vinu <richardnova1999@gmail.com>
2022-09-07 12:51:21 +0530

    added - auhtn,authz

---
+++ src/middleWare/auth.js
@@ -0,0 +1,35 @@
+const jwt = require('jsonwebtoken')
+
+
+let authn = async (req,res,next)=>{
+try{
+let  token =  req.headers['x-api-key']
+
+  if(!token) 
+    return res.send({staus:false,msg:"token enter kar na yarr"})
+
+    let decodedtoken =  jwt.verify(token,"Blogging site Mini Project")
+    req.decoded = decodedtoken
+
+    if(!decodedtoken) 
+    return res.status(401).send({status:false,msg:"invalid-token"})
+
+    next()
+}catch(err){
+    res.status(500).send({msg:err.message})
+} 
+}
+
+
+let authz = async (req,res,next)=>{
+
+   let auhtorId = req.params.auhtorId
+
+if(req.decoded.authorId != auhtorId)
+return res.status(403).send({staus:false,msg:"you are not authorized"})
+
+next()
+}
+
+
+module.exports = {authn,authz}
